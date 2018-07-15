5ad75fbc25cc56b398116b6ca0b9c5248217ddff
amu <carvin_lo@163.com>
2018-07-15 13:44:13 +0800

    開始實作

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+const MemberModifyMethod = require("../controllers/modify_controller");
+const memberModifyMethod = new MemberModifyMethod();
+router.post("/register", memberModifyMethod.postRegister);
 
 module.exports = router;
