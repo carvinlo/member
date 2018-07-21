47a46690e4dc4133b2eaa38fc69c87cefd3ca30d
amu <carvin_lo@163.com>
2018-07-21 14:10:59 +0800

    更新图片

--- controllers/modify_controller.js
+++ controllers/modify_controller.js
@@ -7,6 +7,8 @@ const updateAction = require("../models/update_model");
 const jwt = require("jsonwebtoken");
 const config = require("../config/development_config");
 const check = new Check();
+const formidable = require("formidable");
+const fs = require("fs");
 
 module.exports = class Member {
 	postRegister(req, res, next) {
@@ -140,6 +142,81 @@ module.exports = class Member {
 			});
 		}
 	}
+
+	putUpdateImage(req, res, next) {
+		const form = new formidable.IncomingForm();
+
+		const token = req.headers["token"];
+		//確定token是否有輸入
+		if (check.checkNull(token) === true) {
+			res.json({
+				err: "請輸入token！"
+			});
+		} else if (check.checkNull(token) === false) {
+			verify(token).then(tokenResult => {
+				if (tokenResult === false) {
+					res.json({
+						result: {
+							status: "token錯誤。",
+							err: "請重新登入。"
+						}
+					});
+				} else {
+					form.parse(req, async function(err, fields, files) {
+						// 確認檔案大小是否小於1MB
+						if (check.checkFileSize(files.file.size) === true) {
+							res.json({
+								result: {
+									status: "上傳檔案失敗。",
+									err: "請上傳小於1MB的檔案"
+								}
+							});
+							return;
+						}
+
+						// 確認檔案型態是否為png, jpg, jpeg
+						if (check.checkFileType(files.file.type) === true) {
+							// 將圖片轉成base64編碼
+							const image = await fileToBase64(files.file.path);
+
+							const id = tokenResult;
+
+							// 進行加密
+							const password = encryption(fields.password);
+							const memberUpdateData = {
+								img: image,
+								name: fields.name,
+								password: password,
+								update_date: onTime()
+							};
+
+							updateAction(id, memberUpdateData).then(
+								result => {
+									res.json({
+										result: result
+									});
+								},
+								err => {
+									res.json({
+										result: err
+									});
+								}
+							);
+						} else {
+							res.json({
+								result: {
+									status: "上傳檔案失敗。",
+									err:
+										"請選擇正確的檔案格式。如：png, jpg, jpeg等。"
+								}
+							});
+							return;
+						}
+					});
+				}
+			});
+		}
+	}
 };
 
 //取得現在時間，並將格式轉成YYYY-MM-DD HH:MM:SS
@@ -159,4 +236,12 @@ const onTime = () => {
 		":" + (mi > 9 ? "" : "0") + mi,
 		":" + (ss > 9 ? "" : "0") + ss
 	].join("");
+};
+
+const fileToBase64 = filePath => {
+	return new Promise((resolve, reject) => {
+		fs.readFile(filePath, "base64", function(err, data) {
+			resolve(data);
+		});
+	});
 };
--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,17 @@ router.post("/", function(req, res, next) {
 
 const MemberModifyMethod = require("../controllers/modify_controller");
 const memberModifyMethod = new MemberModifyMethod();
+
+// 註冊新會員
 router.post("/register", memberModifyMethod.postRegister);
 
+// 會員登入
 router.post("/login", memberModifyMethod.postLogin);
 
-router.put('/update', memberModifyMethod.putUpdate);
+// 更新會員資料
+router.put("/update", memberModifyMethod.putUpdate);
+
+// 更新會員資料（檔案上傳示範，可直接取代/member的PUT method）
+router.put("/updateimg", memberModifyMethod.putUpdateImage);
 
 module.exports = router;
